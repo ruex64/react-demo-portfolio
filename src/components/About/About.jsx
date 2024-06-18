c09eb496f01b3a904a1f9969c33c57e4ccc16388
ruex64/Rudraksh Mahajan <112755580+ruex64@users.noreply.github.com>
2024-06-18 20:05:51 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,7 +14,7 @@ function About() {
         </nav>
             <div className="abtcont">
             <h1 className="ahead">{ve}WhoAmI?</h1>
-            <p className="acont">Hey there! Im Rudraksh Mahajan, a B.Tech student in Computer Science and Engineering with a knack for full stack development. From Java and C++ to HTML, CSS, JavaScript, Node.js, and MongoDB, I love crafting sleek, efficient web applications. My tech journey started with a simple curiosity and has evolved into a passion for exploring new languages and technologies. When Im not coding, youll find me behind a camera, capturing the world through my lens and adding a creative flair to my projects. Im all about continuous learning and self-improvement, aiming to be a better developer every day. Based in [Your Location], Im excited to collaborate on innovative projects and connect with fellow tech enthusiasts. Lets build something amazing together!
+            <p className="acont">Hey there! Im Rudraksh Mahajan, a B.Tech student in Computer Science and Engineering with a knack for full stack development. From Java and C++ to HTML, CSS, JavaScript, Node.js, and MongoDB, I love crafting sleek, efficient web applications. My tech journey started with a simple curiosity and has evolved into a passion for exploring new languages and technologies. When Im not coding, youll find me behind a camera, capturing the world through my lens and adding a creative flair to my projects. Im all about continuous learning and self-improvement, aiming to be a better developer every day. Based in Jalandhar, Im excited to collaborate on innovative projects and connect with fellow tech enthusiasts. Lets build something amazing together!
             </p>
         </div>
         </div>
